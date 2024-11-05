1851bdae1a2445a566cf7f25ed598252c3759798
Lucky Goyal <goyallucky2020@gmail.com>
2024-11-05 14:24:24 +0530

    cleanup

--- backend/api/controllers/common/common.go
+++ backend/api/controllers/common/common.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"reflect"
 	"time"
 
 	"github.com/probodevx/engine/shared"
@@ -56,4 +57,22 @@ func PushToQueue(redisClient *redis.Client, queueName string, event interface{},
 	}
 
 	return nil
+}
+
+func GetMapKeys(m interface{}) []string {
+	// Use reflection to check if the input is a map
+	val := reflect.ValueOf(m)
+	if val.Kind() != reflect.Map {
+		return nil
+	}
+
+	// Create a slice to store the keys
+	keys := make([]string, 0, val.Len())
+
+	// Iterate over the map and extract the keys
+	for _, key := range val.MapKeys() {
+		keys = append(keys, key.String())
+	}
+
+	return keys
 }
